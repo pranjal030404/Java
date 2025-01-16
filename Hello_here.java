1cfdd4a60ee40f733debdaa34cba893688e06cc8
pranjal030404 <pranjalp160@gmail.com>
2025-01-17 00:39:44 +0530

    print hello here

--- Hello_here.java
+++ Hello_here.java
@@ -1,5 +1,6 @@
-class Hello_here {
-    public static void main(String[] args) {
-        System.out.println("hello here ");
+// Here we are learning how to print hello world in Java 
+class Hello_here { // For every task in java we have to create a class there
+    public static void main(String[] args) { // this line is mandatory in each and every programm
+        System.out.println("hello here "); // this line is used when we have to print something
     }
 }
