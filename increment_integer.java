4e03d807805c21a945f2a5efe99a3f7ac87e4a61
pranjal030404 <pranjalp160@gmail.com>
2025-01-19 00:37:36 +0530

    incrementing value for integer

---
+++ increment_integer.java
@@ -0,0 +1,11 @@
+// For incrementing any same value for integer 
+
+class increment_integer {
+    public static void main(String[] args) {
+        int age = 30, hello = 20; // integer
+        age = 40;
+
+        System.out.println(age);
+        System.out.println(hello);
+    }
+}
