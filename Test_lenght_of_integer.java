cfa9dbeada2f72f5a8ad656f5f5477b3b6442f8e
pranjal030404 <pranjalp160@gmail.com>
2025-01-21 00:03:28 +0530

    Testing the  Length of the integer

---
+++ Test_lenght_of_integer.java
@@ -0,0 +1,10 @@
+// Testing the length of the integer
+
+public class Test_lenght_of_integer {
+    public static void main(String[] args) {
+        int view = 3_893_312_263; // here we can see that its showing some error in the values of the integer
+                                  // because we are trying to insert values more than 2 Billion and the max value
+                                  // of which can be inserted by the integer is 2B
+        int vew = 123_345_678; // here we have inserted exctaly 2B no so there will be no problem
+    }
+}
