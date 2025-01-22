ec662b799894d4425fc1932d3fa0759e5791d99f
pranjal030404 <pranjalp160@gmail.com>
2025-01-22 11:38:04 +0530

    Testing long datatype

---
+++ long_Datatype.java
@@ -0,0 +1,8 @@
+// Testing a long dataType
+
+class long_Datatype {
+    public static void main(String[] args) {
+        long view = 123_456_789_123L; // L is a suffix for the declaration of long datatype
+        System.out.println(view);
+    }
+}
