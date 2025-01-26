74dc71368ec97628776233fad465e7909e98547c
pranjal030404 <pranjalp160@gmail.com>
2025-01-26 14:58:21 +0530

    current date and time

---
+++ java.current.Date.java
@@ -0,0 +1,10 @@
+// Printing the current date and time
+
+import java.current.Date;
+
+public class Date_time {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println(now);
+    }
+}
