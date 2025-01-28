f1c3d359237515d1f6325558417748d9b9429072
pranjal030404 <pranjalp160@gmail.com>
2025-01-28 15:50:08 +0530

    string_test

---
+++ string_test.java
@@ -0,0 +1,8 @@
+// For printing a string
+
+class string_test {
+    public static void main(String[] args) {
+        String message = "hello world";
+        System.out.println(message);
+    }
+}
