09c3f8f78beda1b429a02b88bdf52b7cad7a2686
pranjal030404 <pranjalp160@gmail.com>
2025-02-10 16:00:20 +0530

    End_with the

---
+++ end_with.java
@@ -0,0 +1,8 @@
+// To check the end with the element 
+class end_with {
+    public static void main(String[] args) {
+        String message = "hello world" + "//";
+        System.out.println(message.endsWith("//")); // it gives result in boolean
+
+    }
+}
