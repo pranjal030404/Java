65994e78a961376f0c476bd34672ff059b7cbd34
pranjal030404 <pranjalp160@gmail.com>
2025-03-17 21:21:48 +0530

    start with

---
+++ start_with.java
@@ -0,0 +1,6 @@
+class start_with {
+    public static void main(String[] args) {
+        String hello = "hello";
+        System.out.println(hello.startsWith("e")); // it gives result in boolean
+    }
+}
