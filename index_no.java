e8d221f478ffe3269becb238f89d1e6a976543cc
pranjal030404 <pranjalp160@gmail.com>
2025-03-23 13:00:00 +0530

    checking the particular index

---
+++ index_no.java
@@ -0,0 +1,8 @@
+// To check index of an particular element
+
+class index_no {
+    public static void main(String[] args) {
+        String index = "Pranjal Tiwari";
+        System.out.println(index.indexOf("T"));
+    }
+}
