82778b21619035f87e9f12742b76ea5ab70bb60f
pranjal030404 <pranjalp160@gmail.com>
2025-03-23 12:57:13 +0530

    updated

--- length_testing.java
+++ length_testing.java
@@ -1,6 +1,8 @@
+// To check the length of the string 
+
 class length_testing {
     public static void main(String[] args) {
         String length = "hello Pranjal";
         System.out.println(length.length());
     }
-} 
+}
