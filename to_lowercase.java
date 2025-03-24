ecdf9f0b59f90634ac956b2a5fd8c23dfff0c2a9
pranjal030404 <pranjalp160@gmail.com>
2025-03-24 12:03:26 +0530

    converting into lower case

---
+++ to_lowercase.java
@@ -0,0 +1,8 @@
+// To lowercase an particular elemenets 
+
+class to_lowercase {
+    public static void main(String[] args) {
+        String lower = "PRANJAL TIWARI";
+        System.out.println(lower.toLowerCase());
+    }
+}
