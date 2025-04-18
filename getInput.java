1c08a3152a2afb6ed9d5da2f12c9d4dbd2928f3d
pranjal030404 <pranjalp160@gmail.com>
2025-04-19 01:06:40 +0530

    getInput

---
+++ getInput.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class getInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        // nextline()
+        // nextint()
+        // nextfloat()
+        // nextdouble()
+        System.out.println(name);
+    }
+}
